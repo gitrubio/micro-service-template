90daa41efad436364ba2e7e9a7f9d6c20140a5fb
deviCarlos <carlos.rubio@devitech.com.co>
2023-02-01 17:04:10 -0500

    nodemon y ts-node

--- api/app.ts
+++ api/app.ts
@@ -3,11 +3,11 @@ import { config } from '../config/global.config'
 import userRoutes from './components/user/user.route'
 
 const app = express()
-
+//midellware
 app.use(express.json())
 app.use(express.urlencoded({ extended : true}))
 
 app.use('api/user',userRoutes)
 app.listen(config.port,()=>{
-    
+    console.log(`servidor corriendo en puerto [ ${config.port} ]`)
 })
